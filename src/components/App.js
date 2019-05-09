4fd6619c6181535618886629d8eff16dc87ee540
Stéphane Dreyfus <floatingrock@gmail.com>
2019-05-08 20:55:22 -0700

    Add empty aboutme files, start navbar and routes in App.js.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { Component} from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import './App.css';
+
+import Home from './Home';
+import AboutMe from './AboutMe';
+
+/** Overall site application:
+ * 
+ * - shows header, nav links, and contains routes to:
+ *  - homepage
+ *  - about_me
+ *  - resume
+ */
 
 function App() {
   return (
+    <div className="app">
+    
+      <header className="app-header">
+        <h1 className="app-welcome"> Welcome to Stéphane's Personal Page</h1>
+        <p className="app-flavor">Dedicating myself to the world: from retreat into development.</p>
+        <nav>
+          <NavLink exact to="/">Home</NavLink>
+          <NavLink exact to="about">About Me</NavLink>
+        </nav>
+      </header>
+
+      <Switch>
+        <Route exact path="/about_me"
+               render={(props) => <AboutMe {...props} />} />
+        <Route exact path="/"
+               render={() => <Home />} />
+      </Switch>
+    </div>
   );
 }
 
