fc181455a93378d237db8f54e33c6b39e39e74a3
Stéphane Dreyfus <floatingrock@gmail.com>
2019-05-12 20:10:54 -0700

    Setup InfoBox basics.

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -3,8 +3,19 @@ import React, { Component } from 'react'
 class InfoBox extends Component {
   render() {
     return (
-      <div>
-        {/* Box title */}
+      <div className="info-box">
+        <div>
+          {/* Box title */}
+        </div>
+        <div>
+          {/* Open curlly brace */}
+        </div>
+        <div>
+          {/* Content */}
+        </div>
+        <div>
+          {/* Close curly brace */}
+        </div>
       </div>
     )
   }
