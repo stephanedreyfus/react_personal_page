9dc554f24dd1f85fc2a54085d017019a83a98578
Stéphane Dreyfus <floatingrock@gmail.com>
2019-05-15 15:51:27 -0700

    CSS work.

--- src/Home.js
+++ src/Home.js
@@ -34,7 +34,7 @@ class Home extends Component {
         <div className="info-links">
           <a href="https://github.com/stephanedreyfus">GitHub</a>
           <a href="https://www.linkedin.com/in/stephane-dreyfus/">LinkedIn</a>
-          <a href="/stephane_dreyfus_resume.pdf">Resume</a>
+          <a href="/public/stephane_dreyfus_resume.pdf">Resume</a>
         </div>
         <InfoBox info={this.props.about}></InfoBox>
         <InfoBox info={this.props.skills}></InfoBox>
