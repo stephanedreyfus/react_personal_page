88a3eff3da33be216e9b2005dbd7421bbb1c8e48
Stéphane Dreyfus <floatingrock@gmail.com>
2019-05-16 09:42:35 -0700

    Updating bootstrap to react-bootstrap.

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -24,7 +24,7 @@ class InfoBox extends Component {
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="" className="btn btn-primary">Go somewhere</a>
+            <a href="/home/" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
       </div>
